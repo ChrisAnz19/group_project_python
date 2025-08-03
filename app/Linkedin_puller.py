bf19f29bdf583cbb80b27b44bc81d0be49f23096
Errol E. <elbasanerrol@gmail.com>
2025-08-03 18:21:29 -0400

    Updated code and refactored

---
+++ app/Linkedin_puller.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+
+def get_linkedin_profile(link_id):
+    # Load environment variables from .env file
+    load_dotenv()
+
+    # Get the API key from the environment
+    api_key = os.getenv("SCRAPINGDOG_API_KEY")
+
+    url = "https://api.scrapingdog.com/linkedin"
+
+    # Set up the request parameters
+    params = {
+        "api_key": api_key,
+        "type": "profile",
+        "linkId": link_id,
+        "premium": "true"
+    }
+
+    # Make the GET request
+    response = requests.get(url, params=params)
+
+    # Check the response and print JSON
+    if response.status_code == 200:
+        data = response.json()
+        return(json.dumps(data, indent=2))
+    else:
+        return(f"Request failed with status code: {response.status_code}")
--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import pymupdf
 from openai import OpenAI
 
@@ -28,40 +28,13 @@ def openai_response(prompt):
         )
     return chat_completion.choices[0].message.content
 
-def get_linkedin_profile(link_id):
-    # Load environment variables from .env file
-    load_dotenv()
-
-    # Get the API key from the environment
-    api_key = os.getenv("SCRAPINGDOG_API_KEY")
-
-    url = "https://api.scrapingdog.com/linkedin"
-
-    # Set up the request parameters
-    params = {
-        "api_key": api_key,
-        "type": "profile",
-        "linkId": link_id,
-        "premium": "true"
-    }
-
-    # Make the GET request
-    response = requests.get(url, params=params)
-
-    # Check the response and print JSON
-    if response.status_code == 200:
-        data = response.json()
-        return(json.dumps(data, indent=2))
-    else:
-        return(f"Request failed with status code: {response.status_code}")
-
 if __name__ == "__main__":
     
     file_path = 'uploads/test.pdf'  # Replace with your PDF file path
     pdf_text = get_pdf_text(file_path)
     print(pdf_text)
 
-    prompt = "Summarize the following text: " + pdf_text
+    prompt = "Provide recommendations for the following resume, break the recommendations into sections for Education, Experience, and Skills: " + pdf_text
     print(openai_response(prompt))
 
     #link_id = input("Enter the LinkedIn linkId (e.g. 'john-doe-1234'): ")
